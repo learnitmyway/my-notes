f90e74e23e6c1d3ccf5fdb53f3de4e0b26ebad5f
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-09-01 16:42:23 +0200

    Import firebase/app instead of the whole sdk in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import firebase from 'firebase'
+import {initializeApp} from 'firebase/app'
 
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
@@ -15,7 +15,7 @@ const config = {
   projectId: `${process.env.REACT_APP_PROJECT_ID}`,
   messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`
 }
-firebase.initializeApp(config)
+initializeApp(config)
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
