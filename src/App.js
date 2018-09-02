ee2034cf6c40aec9c30af59039cd7b601bc7b749
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-09-02 15:08:26 +0200

    3 | Set anonymous uid on mount in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react'
+
+import { signInAnonymously } from './authService'
+
 import './App.css'
 
 class App extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      uid: null
+    }
+  }
+
+  componentDidMount () {
+    signInAnonymously()
+      .then(userCredentials => {
+        this.setState({
+          uid: userCredentials.user.uid
+        })
+      })
+  }
+
   render () {
     return (
       <div className='App' />
--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,29 @@
 /* eslint-env jest */
 
 import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+import App from './App'
+import { signInAnonymously } from './authService'
+
+jest.mock('./authService')
+
+configure({ adapter: new Adapter() })
+
+describe('App', () => {
+  it('Sets anonymous uid on mount', async () => {
+    const expectedUid = 42
+    const userCredential = {
+      user: {
+        uid: expectedUid
+      }
+    }
+
+    signInAnonymously.mockReturnValue(Promise.resolve(userCredential))
+
+    const wrapper = await shallow(<App />)
+
+    expect(wrapper.state().uid).toBe(expectedUid)
+  })
 })
