b3fff9447f836189f5a79a3548bad58364e0dab2
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-12-18 21:46:38 +0100

    9 | Add arrow to not list item and style it

--- src/component/NoteList/NoteListItem/NoteListItem.js
+++ src/component/NoteList/NoteListItem/NoteListItem.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import arrow from './arrow.svg'
+import './NoteListItem.css'
+
 export default function NoteListItem (props) {
   return (
-    <div className='NoteListItem-title'>{props.title}</div>
+    <div className='NoteListItem'>
+      <div className='NoteListItem-title'>{props.title}</div>
+      <img className='NoteList-arrow' src={arrow} alt='right arrow' />
+    </div>
   )
 }
 
