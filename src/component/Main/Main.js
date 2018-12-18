6ec3312db1a914dd9121aa74a099cffcf1ad9113
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-12-18 21:01:45 +0100

    9 | Render note list in main component

--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -2,10 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import CreateNote from '../CreateNote/CreateNote'
+import NoteList from '../NoteList/NoteList'
 
 export default function Main (props) {
   return (
-    <CreateNote uid={props.uid} />
+    <React.Fragment>
+      <CreateNote uid={props.uid} />
+      <NoteList uid={props.uid} />
+    </React.Fragment>
   )
 }
 
--- src/component/Main/Main.test.js
+++ src/component/Main/Main.test.js
@@ -5,11 +5,18 @@ import { shallow } from 'enzyme'
 
 import Main from './Main'
 import CreateNote from '../CreateNote/CreateNote'
+import NoteList from '../NoteList/NoteList'
 
 describe('Main', () => {
-  it('renders create-note button', () => {
+  it('renders create-note component', () => {
     const wrapper = shallow(<Main uid='uid' />)
 
     expect(wrapper.find(CreateNote).length).toBe(1)
+  })
+
+  it('renders note list', () => {
+    const wrapper = shallow(<Main uid='uid' />)
+
+    expect(wrapper.find(NoteList).length).toBe(1)
   })
 })
