1cc81d4d431493a609b09a15e08af71e6207b895
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-12-18 19:47:05 +0100

    9 | Render main component inside app component

--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
 import CreateNote from '../CreateNote/CreateNote'
 
 export default function Main (props) {
   return (
     <CreateNote uid={props.uid} />
   )
+}
+
+Main.propTypes = {
+  uid: PropTypes.string.isRequired
 }
