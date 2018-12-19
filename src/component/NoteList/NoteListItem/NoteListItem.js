c2a8096dfb49060d27e05a0ebd1440096e299a45
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-12-19 17:25:37 +0100

    10 | Remove arrow in note list item

--- src/component/NoteList/NoteListItem/NoteListItem.js
+++ src/component/NoteList/NoteListItem/NoteListItem.js
@@ -2,14 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
-import arrow from './arrow.svg'
 import './NoteListItem.css'
 
 export default function NoteListItem (props) {
   return (
     <Link className='NoteListItem' to={'/' + props.noteId}>
       <div className='NoteListItem-title'>{props.title}</div>
-      <img className='NoteList-arrow' src={arrow} alt='right arrow' />
     </Link>
   )
 }
