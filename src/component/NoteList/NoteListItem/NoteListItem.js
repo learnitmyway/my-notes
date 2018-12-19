cdd9b13ad5d9c18b1c54371cb22c0c2475b999f0
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-12-19 18:38:09 +0100

    10 | Apply styles when selected in NoteListItem

--- src/component/NoteList/NoteListItem/NoteListItem.js
+++ src/component/NoteList/NoteListItem/NoteListItem.js
@@ -5,14 +5,20 @@ import { Link } from 'react-router-dom'
 import './NoteListItem.css'
 
 export default function NoteListItem (props) {
+  let classNames = 'NoteListItem '
+  if (props.isSelected) {
+    classNames += 'NoteListItem--selected'
+  }
+
   return (
-    <Link className='NoteListItem' to={'/' + props.noteId}>
+    <Link className={classNames} to={'/' + props.noteId}>
       <div className='NoteListItem-title'>{props.title}</div>
     </Link>
   )
 }
 
 NoteListItem.propTypes = {
-  title: PropTypes.string.isRequired,
-  noteId: PropTypes.string.isRequired
+  isSelected: PropTypes.bool.isRequired,
+  noteId: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired
 }
--- src/component/NoteList/NoteListItem/NoteListItem.test.js
+++ src/component/NoteList/NoteListItem/NoteListItem.test.js
@@ -8,16 +8,23 @@ import NoteListItem from './NoteListItem'
 describe('NoteListItem', () => {
   it('renders note title', () => {
     const title = 'title'
-    const wrapper = shallow(<NoteListItem title={title} noteId='noteId' />)
+    const wrapper = shallow(<NoteListItem title={title} noteId='noteId' isSelected={false} />)
 
     expect(wrapper.find('.NoteListItem-title').text()).toBe(title)
+    expect(wrapper.find('.NoteListItem--selected').length).toBe(0)
   })
 
   it('navigates to note on click', () => {
     const noteId = 'noteId'
 
-    const wrapper = shallow(<NoteListItem title='title' noteId={noteId} />)
+    const wrapper = shallow(<NoteListItem title='title' noteId={noteId} isSelected={false} />)
 
     expect(wrapper.find('.NoteListItem').props().to).toBe('/' + noteId)
+  })
+
+  it('applies styles when selected', () => {
+    const wrapper = shallow(<NoteListItem title='title' noteId='noteId' isSelected />)
+
+    expect(wrapper.find('.NoteListItem--selected').length).toBe(1)
   })
 })
