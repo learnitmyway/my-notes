f855b55a47727fddeecb55d4ebc144e299b49aee
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-12-21 18:42:50 +0100

    12 | Read note in Container and pass it as a prop

--- src/component/Container/Container.js
+++ src/component/Container/Container.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 
 import Note from '../Note/Note'
 import Main from '../Main/Main'
+import { readNote } from '../../service/noteService/noteService'
 
 import deviceWidths from '../../deviceWidths'
 
@@ -18,8 +19,23 @@ export default class Container extends React.Component {
     this.handleTitleChange = this.handleTitleChange.bind(this)
   }
 
+  readNote () {
+    const successCallback = (snapshot) => {
+      const note = snapshot.val()
+      this.setState({note})
+    }
+
+    readNote(this.props.uid, this.props.match.params.noteId, successCallback)
+  }
+
   handleTitleChange (currentNote) {
     this.setState({currentNote})
+  }
+
+  componentDidMount () {
+    if (this.props.match.params && this.props.match.params.noteId) {
+      this.readNote()
+    }
   }
 
   render () {
@@ -27,12 +43,12 @@ export default class Container extends React.Component {
     const largerScreenContainer = (
       <div className='Container Container--not-small'>
         <Main classNames='Main--not-small' currentNote={currentNote} uid={this.props.uid} match={this.props.match} />
-        <Note classNames='Note--not-small' onTitleChange={this.handleTitleChange} {...this.props} />
+        <Note classNames='Note--not-small' note={this.state.note} onTitleChange={this.handleTitleChange} {...this.props} />
       </div>
     )
     return (
       window.innerWidth < deviceWidths.small
-        ? <Note onTitleChange={() => {}} {...this.props} />
+        ? <Note note={this.state.note} onTitleChange={() => {}} {...this.props} />
         : largerScreenContainer
     )
   }
--- src/component/Container/Container.test.js
+++ src/component/Container/Container.test.js
@@ -6,6 +6,9 @@ import { shallow } from 'enzyme'
 import Container from './Container'
 import Note from '../Note/Note'
 import Main from '../Main/Main'
+import { readNote } from '../../service/noteService/noteService'
+
+jest.mock('../../service/noteService/noteService')
 
 describe('Container', () => {
   it('renders note component', () => {
@@ -23,6 +26,27 @@ describe('Container', () => {
     expect(mainComponentProps.classNames).toBe('Main--not-small')
     expect(mainComponentProps.match).toBe(match)
     expect(wrapper.find(Note).props().classNames).toBe('Note--not-small')
+  })
+
+  it('reads note and passes it as a prop', () => {
+    const expectedNote = {
+      title: 'title',
+      body: 'body'
+    }
+    const snapshot = {
+      val: function () {
+        return expectedNote
+      }
+    }
+    readNote.mockImplementation((uid, noteId, cb) => {
+      cb(snapshot)
+    })
+
+    const match = {params: {noteId: 'noteId'}}
+    const wrapper = shallow(<Container uid='uid' match={match} />)
+
+    expect(wrapper.find(Note).props().note).toBe(expectedNote)
+    expect(wrapper.find(Note).props().match).toBe(match)
   })
 
   it('passes current note as prop on note title change', () => {
