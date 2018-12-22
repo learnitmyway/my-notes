c76745083c4be622e5d0bb162e842604e276f8e8
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-12-22 19:57:43 +0100

    12 | Pass note down to Note

--- src/component/Note/Note.js
+++ src/component/Note/Note.js
@@ -1,101 +1,48 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import ContentEditable from 'react-contenteditable'
 
-import { readNote } from '../../service/noteService/noteService'
-
 import './Note.css'
 
-export default class Note extends Component {
-  constructor (props) {
-    super(props)
-    this.state = { }
-
-    this.handleTitleChange = this.handleTitleChange.bind(this)
-    this.handleBodyChange = this.handleBodyChange.bind(this)
+export default function Note (props) {
+  function handleTitleChange (e) {
+    props.onTitleChange(e.target.value)
   }
 
-  renderErrorMessage () {
-    this.setState({
-      isError: true
-    })
+  function handleBodyChange (e) {
+    props.onBodyChange(e.target.value)
   }
 
-  readNote () {
-    const successCallback = (snapshot) => {
-      const note = snapshot.val()
-
-      this.setState({
-        title: note.title,
-        body: note.body
-      })
-    }
-
-    const failureCallback = () => {}
-
-    readNote(this.props.uid, this.props.match.params.noteId, successCallback, failureCallback)
+  let classNames = 'Note '
+  if (props.classNames) {
+    classNames += props.classNames
   }
 
-  handleTitleChange (e) {
-    this.setState({title: e.target.value})
-    this.props.onTitleChange(e.target.value)
-  }
+  const shouldRenderTitle = props.note && (props.note.title || props.note.title === '')
+  const shouldRenderBody = props.note && (props.note.body || props.note.body === '')
 
-  handleBodyChange (e) {
-    this.setState({body: e.target.value})
-    this.props.onBodyChange(e.target.value)
-  }
-
-  componentDidUpdate (prevProps, prevState, snapshot) {
-    if (this.props.match.params.noteId !== prevProps.match.params.noteId) {
-      this.readNote()
-    }
-  }
-
-  componentDidMount () {
-    if (this.props.match.params && this.props.match.params.noteId) {
-      this.readNote()
-    }
-  }
-
-  render () {
-    let classNames = 'Note '
-    if (this.props.classNames) {
-      classNames += this.props.classNames
-    }
-
-    const shouldRenderTitle = this.state.title || this.state.title === ''
-    const shouldRenderBody = this.state.body || this.state.body === ''
-
-    return (
-      <div className={classNames}>
-        {shouldRenderTitle && <ContentEditable className='Note-title'
-          html={this.state.title}
-          onChange={this.handleTitleChange}
-        />}
-        {shouldRenderBody && <ContentEditable className='Note-body'
-          html={this.state.body}
-          onChange={this.handleBodyChange}
-        />}
-        {this.props.isError && <div className='Note-error'>Note cannot be found</div>}
-      </div>
-    )
-  }
+  return (
+    <div className={classNames}>
+      {shouldRenderTitle && <ContentEditable className='Note-title'
+        html={props.note.title}
+        onChange={handleTitleChange}
+      />}
+      {shouldRenderBody && <ContentEditable className='Note-body'
+        html={props.note.body}
+        onChange={handleBodyChange}
+      />}
+      {props.isError && <div className='Note-error'>Note cannot be found</div>}
+    </div>
+  )
 }
 
 Note.propTypes = {
+  classNames: PropTypes.string,
   isError: PropTypes.bool.isRequired,
   note: PropTypes.shape({
     title: PropTypes.string,
     body: PropTypes.string
   }).isRequired,
   onBodyChange: PropTypes.func.isRequired,
-  onTitleChange: PropTypes.func.isRequired,
-  uid: PropTypes.string.isRequired,
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      noteId: PropTypes.string
-    })
-  }).isRequired,
-  classNames: PropTypes.string
+  onTitleChange: PropTypes.func.isRequired
 }
--- src/component/Note/Note.test.js
+++ src/component/Note/Note.test.js
@@ -40,11 +40,9 @@ describe('Note', () => {
     expect(wrapper.find('.Note-error').length).toBe(0)
   })
 
-  it('renders empty note when there is no note id in url path', () => {
-    const match = {params: {}}
-    const wrapper = shallow(<Note {...initialProps} match={match} />)
+  it('renders empty note when there is no note', () => {
+    const wrapper = shallow(<Note {...initialProps} note={{}} />)
 
-    expect(readNote).not.toHaveBeenCalled()
     expect(wrapper.find(ContentEditable).length).toBe(0)
     expect(wrapper.find('.Note-error').length).toBe(0)
   })
@@ -55,74 +53,29 @@ describe('Note', () => {
     expect(wrapper.find('.Note-error').text()).toBe('Note cannot be found')
   })
 
-  it('reads new note when note id changes', () => {
-    const uid = 'someUid'
-    const noteId1 = 'noteId1'
-    const match1 = {params: {noteId: noteId1}}
-    const prevProps = {uid, match: match1}
-    const wrapper = shallow(<Note {...initialProps} {...prevProps} />)
-
-    const noteId2 = 'noteId2'
-    const match2 = {params: {noteId: noteId2}}
-    const props = {uid, match: match2}
-    wrapper.setProps(props)
-
-    expect(readNote).toHaveBeenCalledWith(uid, noteId2, expect.any(Function), expect.any(Function))
-  })
-
   it('applies class names from props', () => {
     const wrapper = shallow(<Note {...initialProps} classNames='forty-two' />)
 
     expect(wrapper.find('.forty-two').length).toBe(1)
   })
 
-  it('updates title on change and handles it', () => {
-    const body = 'body'
-    const note = {
-      title: 'title',
-      body
-    }
-    const snapshot = {
-      val: function () {
-        return note
-      }
-    }
-    readNote.mockImplementation((uid, noteId, cb) => {
-      cb(snapshot)
-    })
-
+  it('handles title change', () => {
     const handleTitleChange = jest.fn()
     const wrapper = shallow(<Note {...initialProps} onTitleChange={handleTitleChange} />)
 
     const newTitle = 'new title'
-    wrapper.find(ContentEditable).at(0).prop('onChange')({target: {value: newTitle}})
+    wrapper.find(ContentEditable).at(0).props().onChange({target: {value: newTitle}})
 
-    expect(wrapper.find(ContentEditable).at(0).props().html).toBe(newTitle)
     expect(handleTitleChange).toHaveBeenCalledWith(newTitle)
   })
 
-  it('updates body on change and handles it', () => {
-    const title = 'title'
-    const note = {
-      title,
-      body: 'body'
-    }
-    const snapshot = {
-      val: function () {
-        return note
-      }
-    }
-    readNote.mockImplementation((uid, noteId, cb) => {
-      cb(snapshot)
-    })
-
+  it('handles body change', () => {
     const handleBodyChange = jest.fn()
     const wrapper = shallow(<Note {...initialProps} onBodyChange={handleBodyChange} />)
 
     const newBody = 'new body'
     wrapper.find(ContentEditable).at(1).prop('onChange')({target: {value: newBody}})
 
-    expect(wrapper.find(ContentEditable).at(1).props().html).toBe(newBody)
     expect(handleBodyChange).toHaveBeenCalledWith(newBody)
   })
 })
