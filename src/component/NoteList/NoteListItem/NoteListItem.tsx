a366ca50923904e3210b4ebeb8c920a75981f96c
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-04-03 12:29:39 +0200

    Convert NoteListItem to tsx

---
+++ src/component/NoteList/NoteListItem/NoteListItem.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './NoteListItem.css'
+
+export interface Props {
+  isSelected: boolean
+  noteId: string
+  title: string
+}
+
+export default function NoteListItem(props: Props) {
+  const { isSelected, noteId, title } = props
+  let classNames = 'NoteListItem '
+  if (isSelected) {
+    classNames += 'NoteListItem--selected'
+  }
+
+  return (
+    <Link className={classNames} to={'/' + noteId}>
+      <div className="NoteListItem-title">{title}</div>
+    </Link>
+  )
+}
