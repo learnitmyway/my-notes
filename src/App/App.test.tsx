1b017e85f640bc46ce53f81ca5cb431e0ed8b591
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-04-06 18:21:14 +0200

    Test creating a new note for smaller devices

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,48 +1,106 @@
 import React from 'react'
-import { fireEvent, waitForElement } from 'react-testing-library'
+import { fireEvent } from 'react-testing-library'
 import uuidv1 from 'uuid/v1'
 import { renderWithRouter } from '../testUtils/renderWithRouter'
 
+import sidebarStyles from '../Sidebar/Sidebar.module.css'
+import App from './App'
+
 import { signInAnonymously } from '../authService/authService'
 import { createNote, readNote } from '../noteService/noteService'
-import App from './App'
 
 jest.mock('uuid/v1')
 jest.mock('../noteService/noteService')
 jest.mock('../authService/authService')
 
 describe('App', () => {
-  it('creates a new note with title and body and navigates to it', async () => {
-    const expectedNoteId = 'noteId'
-    uuidv1.mockReturnValue(expectedNoteId)
-
-    const expectedUid = '42'
-    const userCredential = {
-      user: {
-        uid: expectedUid
-      }
-    }
-
-    signInAnonymously.mockReturnValue(Promise.resolve(userCredential))
-
-    const expectedTitle = 'title'
-    const expectedBody = 'body'
-    const snapshot = {
-      val() {
-        return { title: expectedTitle, body: expectedBody }
-      }
-    }
-    readNote.mockImplementation((uid, noteId, cb) => {
-      cb(snapshot)
+  describe('for small devices', () => {
+    beforeEach(() => {
+      window.innerWidth = 599
     })
 
-    const { history, getByTestId } = await renderWithRouter(<App />)
+    describe('at root path', () => {
+      it('creates a new note with title and body and navigates to it. Closes sidebar', async () => {
+        const expectedNoteId = 'noteId'
+        uuidv1.mockReturnValue(expectedNoteId)
 
-    fireEvent.click(getByTestId('CreateNote__btn'))
+        const expectedUid = '42'
+        const userCredential = {
+          user: {
+            uid: expectedUid
+          }
+        }
 
-    expect(createNote).toHaveBeenCalledWith(expectedUid, expectedNoteId)
-    expect(history.entries[1].pathname).toBe('/' + expectedNoteId)
-    expect(getByTestId('Note__title').value).toBe(expectedTitle)
-    expect(getByTestId('Note__body').value).toBe(expectedBody)
+        signInAnonymously.mockReturnValue(Promise.resolve(userCredential))
+
+        const expectedTitle = 'title'
+        const expectedBody = 'body'
+        const snapshot = {
+          val() {
+            return { title: expectedTitle, body: expectedBody }
+          }
+        }
+        readNote.mockImplementation((uid, noteId, cb) => {
+          cb(snapshot)
+        })
+
+        const { container, getByTestId, history } = await renderWithRouter(
+          <App />
+        )
+
+        fireEvent.click(getByTestId('CreateNote__btn'))
+
+        expect(createNote).toHaveBeenCalledWith(expectedUid, expectedNoteId)
+        expect(history.entries[1].pathname).toBe('/' + expectedNoteId)
+        expect(getByTestId('Note__title').value).toBe(expectedTitle)
+        expect(getByTestId('Note__body').value).toBe(expectedBody)
+        expect(container.querySelector(`.${sidebarStyles.open}`)).toBeNull()
+      })
+    })
+  })
+
+  describe('for non-small devices', () => {
+    beforeEach(() => {
+      window.innerWidth = 600
+    })
+
+    describe('at root path', () => {
+      it('creates a new note with title and body and navigates to it. Does not close sidebar', async () => {
+        const expectedNoteId = 'noteId'
+        uuidv1.mockReturnValue(expectedNoteId)
+
+        const expectedUid = '42'
+        const userCredential = {
+          user: {
+            uid: expectedUid
+          }
+        }
+
+        signInAnonymously.mockReturnValue(Promise.resolve(userCredential))
+
+        const expectedTitle = 'title'
+        const expectedBody = 'body'
+        const snapshot = {
+          val() {
+            return { title: expectedTitle, body: expectedBody }
+          }
+        }
+        readNote.mockImplementation((uid, noteId, cb) => {
+          cb(snapshot)
+        })
+
+        const { container, history, getByTestId } = await renderWithRouter(
+          <App />
+        )
+
+        fireEvent.click(getByTestId('CreateNote__btn'))
+
+        expect(createNote).toHaveBeenCalledWith(expectedUid, expectedNoteId)
+        expect(history.entries[1].pathname).toBe('/' + expectedNoteId)
+        expect(getByTestId('Note__title').value).toBe(expectedTitle)
+        expect(getByTestId('Note__body').value).toBe(expectedBody)
+        expect(container.querySelector(`.${sidebarStyles.open}`)).not.toBeNull()
+      })
+    })
   })
 })
