5525e06f373b4701cb613000dbe455a4db0214ec
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-09-04 16:16:32 +0200

    Add Feedback form

--- src/Feedback/Feedback.js
+++ src/Feedback/Feedback.js
@@ -1,5 +1,49 @@
 import React from 'react'
+import { Formik } from 'formik'
 
 export default function Feedback() {
-  return <p>Feedback</p>
+  return (
+    <section>
+      <h1>Feedback</h1>
+      <Formik
+        initialValues={{ text: '' }}
+        validate={values => {
+          let errors = {}
+          if (!values.text) {
+            errors.text = 'Required'
+          }
+          return errors
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          console.log(values)
+          setSubmitting(false)
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting
+          /* and other goodies */
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="text"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.text}
+            />
+            {errors.text && touched.text && errors.text}
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </form>
+        )}
+      </Formik>
+    </section>
+  )
 }
